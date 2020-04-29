4a2b6a0d6d9c584718a3c4fbdeb49e6f6338cc24
Artur Barsegyan <a.barsegyan@corp.mail.ru>
2020-04-29 18:00:28 +0300

    add stat operation for incr queue

--- serv.c
+++ serv.c
@@ -4,51 +4,51 @@
 #include <sys/msg.h>
 #include <string.h>
 #include <stdio.h>
-#include  <signal.h>
+#include <signal.h>
 #include <stdlib.h>
 
 int my_msg_count=0;
 
+void my_handler(int sig) {
+    printf("pid=%d count=%d\n", getpid(), my_msg_count);
+    my_msg_count=0;
 
-void  my_handler(int sig)
-{
-
-  printf("pid=%d count=%d\n",getpid(),my_msg_count);
-  my_msg_count=0;
-
-   alarm(1);
+    alarm(1);
 }
 
-
-
-
-int main()
-{
+int main() {
     int msqid;
 
     key_t key=0x112233;
     int len, maxlen;
-    struct mymsgbuf
-    {
+    struct mymsgbuf {
         long mtype;
         char mtext[81];
     } mybuf;
+
     signal(SIGALRM, my_handler);
-   alarm(1);
+    alarm(1);
 
     if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0){
         printf("Can\'t get msqid\n");
         exit(-1);
     }
-    while(1){
 
-        maxlen = 181;
-        if((len = msgrcv(msqid,
-            (struct msgbuf *) &mybuf, maxlen, 0, 0)) >0 ){
+    struct msqid_ds buf;
+    msgctl(msqid, IPC_STAT, buf);
 
-	my_msg_count++;
+    // The maximum number of outstanding messages in queue
+    buf.msg_tql = 100;
+
+    // The maximum number of bytes allowed in messages outstanding on the associated message queue.
+    buf.msg_qbytes = 2750;
+    msgctl(msqid, IPC_SET, buf);
+
+    while(1) {
+        maxlen = 81;
+        if ((len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 0, 0)) > 0){
+	       my_msg_count++;
         }
-
     }
     return 0;
 }
